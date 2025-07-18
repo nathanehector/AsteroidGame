b2ddaa277fe11af419743cdcca32b3e7559acd78
Naktin347 <nathanehector@gmail.com>
2025-07-17 23:18:47 -0500

    Asteroid Game

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
-
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS, PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import *
+from shot import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -8,14 +11,54 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    dt= 0
+
+
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+
+    Player.containers = (updatables, drawables)
+    Asteroid.containers = (asteroids, updatables, drawables)
+    AsteroidField.containers = (updatables,)
+    Shot.containers = (shots, updatables, drawables)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+
+    
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill((0,0,0))
-        pygame.display.flip()
 
+        for obj in updatables:
+            obj.update(dt)
+        
+   
+        for asteroid in asteroids:
+            if player.collides_with(asteroid):
+                print("Game over!")
+                pygame.quit()
+                exit()
+            
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                     asteroid.split()
+                     shot.kill()
+
+        for obj in drawables:
+            obj.draw(screen)
+        
+        pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
